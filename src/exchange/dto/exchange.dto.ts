d529d21b9127ea5731f679f8fcaa40d10a74bf89
Ruslan Bondarenko <ruslan.bondarenko@kilo.health>
2024-04-04 09:44:09 +0200

    add exchange dto with validation for controller and response dto

--- src/exchange/dto/exchange.dto.ts
+++ src/exchange/dto/exchange.dto.ts
@@ -1,5 +1,15 @@
+import {IsNotEmpty, IsNumber} from 'class-validator';
+
 export class ExchangeDto {
+  @IsNumber()
+  @IsNotEmpty()
   from: number;
+
+  @IsNumber()
+  @IsNotEmpty()
   to: number;
+
+  @IsNumber()
+  @IsNotEmpty()
   amount: number;
 }
--- src/exchange/exchange.controller.ts
+++ src/exchange/exchange.controller.ts
@@ -1,18 +1,20 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ExchangerService } from './exchanger.service';
 import { ExchangeDto } from './dto/exchange.dto';
+import {SuccessResponse} from "../dto/success.response";
 
 @Controller('exchanges')
 export class ExchangeController {
   constructor(private readonly exchangerService: ExchangerService) {}
 
   @Post('/')
-  public async exchange(@Body() exchangeDto: ExchangeDto): Promise<number> {
-    //@todo response and validation
-    return this.exchangerService.exchange(
+  public async exchange(@Body() exchangeDto: ExchangeDto): Promise<SuccessResponse<any>> {
+    const value = await this.exchangerService.exchange(
       exchangeDto.from,
       exchangeDto.to,
       exchangeDto.amount,
     );
+
+    return new SuccessResponse<any>({"value": value})
   }
 }
