0ec115f0ae994883de5f8ef21b43bc4c47c836e5
Ruslan Bondarenko <ruslan.bondarenko@kilo.health>
2024-04-04 09:43:24 +0200

    add config module to app module  and redis module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ExchangeModule } from './exchange/exchange.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule } from './config/config.module';
+import { AppConfig } from './config/app.config';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [ExchangeModule],
+  imports: [
+    ConfigModule,
+    ExchangeModule,
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async (appConfig: AppConfig) => ({
+        store: redisStore,
+        host: appConfig.redis.host,
+        port: appConfig.redis.port,
+      }),
+      imports: [ConfigModule],
+      inject: [AppConfig],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
